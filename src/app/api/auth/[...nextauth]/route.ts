cc3ec893bd00ba5a33bcfeb0e94816dfec795665
neogmanish99 <sanjuneog399@gmail.com>
2023-05-30 11:39:32 +0530

    next auth commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,8 +6,12 @@ export const authOptions: NextAuthOptions = {
     providers: [
         // ...add more providers here
         CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
             name: "Credentials",
-
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
                 email: {
                     label: "Email",
@@ -41,6 +45,10 @@ export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
+
+    pages: {
+        signIn: "/auth/login",
+    },
 };
 
-export default authOptions;
+export default NextAuth(authOptions);
